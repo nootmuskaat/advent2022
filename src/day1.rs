1c3f17e7b5567540885222f7213c90c07bd0e866
nootmuskaat <6219341+nootmuskaat@users.noreply.github.com>
2023-06-19 15:14:18 +0200

    Complete day 1 part 2

--- src/day1.rs
+++ src/day1.rs
@@ -1,3 +1,5 @@
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
 use std::env;
 use std::fs;
 
@@ -8,6 +10,7 @@ fn filename() -> String {
     f
 }
 
+#[allow(dead_code)]
 fn most_calories(filepath: &str) -> Result<usize, std::io::Error> {
     let mut count: usize = 0;
     let mut max_found: usize = 0;
@@ -26,10 +29,33 @@ fn most_calories(filepath: &str) -> Result<usize, std::io::Error> {
     Ok(max_found)
 }
 
+fn top_3_calories(filepath: &str) -> Result<usize, std::io::Error> {
+    let mut most_calories: BinaryHeap<Reverse<usize>> = BinaryHeap::with_capacity(4);
+    let mut count: usize = 0;
+    fs::read_to_string(&filepath)?
+        .split("\n")
+        .chain(std::iter::once("")) // ensure we can push the last grouping
+        .map(|line| line.parse::<usize>())
+        .for_each(|line| match line {
+            Ok(i) => {
+                count += i;
+            }
+            Err(_) => {
+                most_calories.push(Reverse(count));
+                if most_calories.len() > 3 {
+                    _ = most_calories.pop();
+                }
+                count = 0;
+            }
+        });
+    Ok(most_calories.iter().fold(0, |total, i| total + i.0))
+}
+
 pub fn main() {
     let f = filename();
-    if let Ok(most_calories) = most_calories(&f) {
-        println!("The most calories available: {}", most_calories);
+    // if let Ok(most_calories) = most_calories(&f) {
+    if let Ok(most_calories) = top_3_calories(&f) {
+        println!("The most calories available: {:?}", most_calories);
     } else {
         eprintln!("Failed to read file {}", f);
     }
