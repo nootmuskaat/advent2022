85392b73ea1c7870cbeca82c87c333c63ad06e9b
nootmuskaat <6219341+nootmuskaat@users.noreply.github.com>
2023-06-26 14:46:04 +0200

    Begin day 4

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 // pub mod day1;
 // pub mod day2;
-pub mod day3;
+// pub mod day3;
+pub mod day4;
 
 
 pub fn filename() -> String {
@@ -12,5 +13,5 @@ pub fn filename() -> String {
 
 fn main() {
     let f = filename();
-    day3::part2_main(&f);
+    day4::day_main(&f);
 }
