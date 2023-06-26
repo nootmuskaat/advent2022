583a1d507a834a72a3e1198b74aee0f60efde29f
nootmuskaat <6219341+nootmuskaat@users.noreply.github.com>
2023-06-26 15:37:39 +0200

    Complete day 4 part 1 with struct

--- src/day4.rs
+++ src/day4.rs
@@ -7,21 +7,10 @@ pub fn day_main(filename: &str) {
 
     for line in lines {
         if let Ok(line) = line {
-            if let Some((a, b)) = line.split_once(",") {
-                if let (Some((a_start, a_end)), Some((b_start, b_end))) =
-                    (a.split_once("-"), b.split_once("-"))
-                {
-                    if let (Ok(a_start), Ok(a_end), Ok(b_start), Ok(b_end)) = (
-                        a_start.parse::<usize>(),
-                        a_end.parse::<usize>(),
-                        b_start.parse::<usize>(),
-                        b_end.parse::<usize>(),
-                    ) {
-                        if identify_problematic_pairings(a_start, a_end, b_start, b_end) {
-                            count += 1;
-                            println!("{} - {} <==> {} - {}", a_start, a_end, b_start, b_end);
-                        }
-                    }
+            if let Some(pair) = Pairing::from_str(&line) {
+                if pair.is_overlapping() {
+                    count += 1;
+                    println!("Overlapping: {:?}", pair);
                 }
             }
         }
@@ -29,11 +18,48 @@ pub fn day_main(filename: &str) {
     println!("Final count is {}", count);
 }
 
-fn identify_problematic_pairings(
-    a_start: usize,
-    a_end: usize,
-    b_start: usize,
-    b_end: usize,
-) -> bool {
-    (a_start <= b_start && a_end >= b_end) || (a_start >= b_start && a_end <= b_end)
+#[derive(Debug)]
+struct Assignment {
+    start: usize,
+    end: usize,
+}
+
+#[derive(Debug)]
+struct Pairing {
+    a_side: Assignment,
+    b_side: Assignment,
+}
+
+impl Pairing {
+    pub fn new(a_start: usize, a_end: usize, b_start: usize, b_end: usize) -> Self {
+        Self {
+            a_side: Assignment {
+                start: a_start,
+                end: a_end,
+            },
+            b_side: Assignment {
+                start: b_start,
+                end: b_end,
+            },
+        }
+    }
+
+    pub fn from_str(s: &str) -> Option<Self> {
+        let (a, b) = s.split_once(",")?;
+        let (a_start, a_end) = a.split_once("-")?;
+        let (b_start, b_end) = b.split_once("-")?;
+        if let (Ok(a_start), Ok(a_end)) = (a_start.parse::<usize>(), a_end.parse::<usize>()) {
+            if let (Ok(b_start), Ok(b_end)) = (b_start.parse::<usize>(), b_end.parse::<usize>()) {
+                return Some(Self::new(a_start, a_end, b_start, b_end));
+            }
+        }
+        None
+    }
+
+    fn is_overlapping(&self) -> bool {
+        // a encompasses b
+        (self.a_side.start <= self.b_side.start && self.a_side.end >= self.b_side.end)
+        // b encompasses a
+            || (self.a_side.start >= self.b_side.start && self.a_side.end <= self.b_side.end)
+    }
 }
