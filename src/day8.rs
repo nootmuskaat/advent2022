8742892585a550b27878b52100a9c2ae1328a780
nootmuskaat <6219341+nootmuskaat@users.noreply.github.com>
2023-07-07 17:55:56 +0200

    Refactor of day 8 part 1

--- src/day8.rs
+++ src/day8.rs
@@ -1,94 +1,102 @@
-use std::io::{BufRead, BufReader};
 use std::collections::HashSet;
+use std::io::{BufRead, BufReader};
 
 type Row = Vec<u8>;
+type Coord = (usize, usize);
+type CurrentTallest = Option<u8>;
 const TALLEST_POSSIBLE: u8 = 9;
+enum Found {
+    Nothing,
+    Taller(u8, Coord),
+    Tallest(Coord),
+}
 
 pub fn day_main(filename: &str) {
     let f = std::fs::File::open(filename).expect("Unable to open file");
     let mut lines = BufReader::new(f).lines();
     let matrix = create_matrix(lines);
     // println!("{:?}", &matrix);
-    let mut visible: HashSet<(usize, usize)> = HashSet::with_capacity(128);
-    let mut tallest: Option<&u8> = None;
+    let mut visible: HashSet<Coord> = HashSet::with_capacity(128);
+    let mut current_tallest: CurrentTallest = None;
     for row in 0..matrix.len() {
-        tallest = None;
+        current_tallest = None;
         for col in 0..matrix[row].len() {
-            match tallest {
-                None => {
-                    visible.insert((row, col));
-                    tallest = Some(&matrix[row][col]);
+            match nameme(&matrix, (row, col), &current_tallest) {
+                Found::Nothing => {}
+                Found::Taller(n, c) => {
+                    visible.insert(c);
+                    current_tallest = Some(n);
                 }
-                Some(&tall) => {
-                    if matrix[row][col] > tall {
-                        visible.insert((row, col));
-                        tallest = Some(&matrix[row][col]);
-                    }
+                Found::Tallest(c) => {
+                    visible.insert(c);
+                    break;
                 }
-            }
-            if *tallest.unwrap() == TALLEST_POSSIBLE {
-                break;
             }
         }
-        tallest = None;
+        current_tallest = None;
         for col in (0..matrix[row].len()).rev() {
-            match tallest {
-                None => {
-                    visible.insert((row, col));
-                    tallest = Some(&matrix[row][col]);
+            match nameme(&matrix, (row, col), &current_tallest) {
+                Found::Nothing => {}
+                Found::Taller(n, c) => {
+                    visible.insert(c);
+                    current_tallest = Some(n);
                 }
-                Some(&tall) => {
-                    if matrix[row][col] > tall {
-                        visible.insert((row, col));
-                        tallest = Some(&matrix[row][col]);
-                    }
+                Found::Tallest(c) => {
+                    visible.insert(c);
+                    break;
                 }
-            }
-            if *tallest.unwrap() == TALLEST_POSSIBLE {
-                break;
             }
         }
     }
     for col in (0..matrix[0].len()).rev() {
-        tallest = None;
+        current_tallest = None;
         for row in 0..matrix.len() {
-            match tallest {
-                None => {
-                    visible.insert((row, col));
-                    tallest = Some(&matrix[row][col]);
+            match nameme(&matrix, (row, col), &current_tallest) {
+                Found::Nothing => {}
+                Found::Taller(n, c) => {
+                    visible.insert(c);
+                    current_tallest = Some(n);
                 }
-                Some(&tall) => {
-                    if matrix[row][col] > tall {
-                        visible.insert((row, col));
-                        tallest = Some(&matrix[row][col]);
-                    }
+                Found::Tallest(c) => {
+                    visible.insert(c);
+                    break;
                 }
-            }
-            if *tallest.unwrap() == TALLEST_POSSIBLE {
-                break;
             }
         }
-        tallest = None;
+        current_tallest = None;
         for row in (0..matrix.len()).rev() {
-            match tallest {
-                None => {
-                    visible.insert((row, col));
-                    tallest = Some(&matrix[row][col]);
+            match nameme(&matrix, (row, col), &current_tallest) {
+                Found::Nothing => {}
+                Found::Taller(n, c) => {
+                    visible.insert(c);
+                    current_tallest = Some(n);
                 }
-                Some(&tall) => {
-                    if matrix[row][col] > tall {
-                        visible.insert((row, col));
-                        tallest = Some(&matrix[row][col]);
-                    }
+                Found::Tallest(c) => {
+                    visible.insert(c);
+                    break;
                 }
-            }
-            if *tallest.unwrap() == TALLEST_POSSIBLE {
-                break;
             }
         }
     }
     println!("{:?}", visible.len());
+}
 
+fn nameme(matrix: &Vec<Row>, coord: Coord, current_tallest: &CurrentTallest) -> Found {
+    let (row, col) = coord;
+    if matrix[row][col] == TALLEST_POSSIBLE {
+        return Found::Tallest(coord);
+    }
+    match *current_tallest {
+        None => {
+            return Found::Taller(matrix[row][col], coord);
+        }
+        Some(tall) => {
+            if matrix[row][col] > tall {
+                return Found::Taller(matrix[row][col], coord);
+            }
+        }
+    }
+    Found::Nothing
 }
 
 fn create_matrix<E>(lines: impl Iterator<Item = Result<String, E>>) -> Vec<Row> {
